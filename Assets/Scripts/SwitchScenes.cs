5be45eaf54dcc2a64330eb778846a9f134cf780f
Ethan Grossbach <egrossba@ucsc.edu>
2020-04-08 18:16:10 -0700

    Attempt to fix triggers
    The attempt failed

--- Assets/Scripts/SwitchScenes.cs
+++ Assets/Scripts/SwitchScenes.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScenes : MonoBehaviour
 {
-    public int goTo;
+    public int goTo, x, y;
+    private GameObject player; 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene(goTo);
+        player = GameObject.FindWithTag("Player");
+        player.transform.position = new Vector2(x, y);
+        Debug.Log(player.transform.position);
     }
+
 }
