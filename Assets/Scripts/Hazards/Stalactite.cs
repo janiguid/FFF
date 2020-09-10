6e938f88a2b9badd372e9050593f228aea094102
Jessie Aniguid <jessieaniguid@gmail.com>
2020-09-10 14:34:01 -0700

    Stalactite pooler in

--- Assets/Scripts/Hazards/Stalactite.cs
+++ Assets/Scripts/Hazards/Stalactite.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stalagmite : Hazard
+public class Stalactite : Hazard
 {
     [SerializeField] float xForceMultiplier;
     [SerializeField] float yForceMultiplier;
@@ -15,7 +15,7 @@ public class Stalagmite : Hazard
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             IDamageable damageable;
             IPushable pushable;
@@ -24,7 +24,7 @@ public class Stalagmite : Hazard
 
             if (collision.gameObject.TryGetComponent<IDamageable>(out damageable))
             {
-                
+
                 damageable.ApplyDamage(damage);
             }
             else
@@ -37,7 +37,7 @@ public class Stalagmite : Hazard
                 Vector3 myCenter = GetComponent<SpriteRenderer>().bounds.center;
                 float xForce = collision.transform.position.x - myCenter.x;
                 float yForce = collision.transform.position.y - myCenter.y;
-                pushable.ApplyForce(xForce*xForceMultiplier, yForce*yForceMultiplier);
+                pushable.ApplyForce(xForce * xForceMultiplier, yForce * yForceMultiplier);
                 Debug.DrawLine(myCenter, collision.transform.position, Color.red, 5f);
             }
             else
@@ -46,5 +46,8 @@ public class Stalagmite : Hazard
             }
 
         }
+
+        gameObject.SetActive(false);
     }
+
 }
---
+++ Assets/Scripts/Hazards/StalactitePooler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactitePooler : MonoBehaviour
+{
+    [SerializeField] private float spawnTimer;
+    [SerializeField] private GameObject baseStalactite;
+    [SerializeField] private List<GameObject> stalactitePool;
+
+    WaitForSeconds wait;
+    private void Start()
+    {
+        wait = new WaitForSeconds(spawnTimer);
+
+        GameObject temp;
+        stalactitePool = new List<GameObject>();
+        for(int i = 0; i < 4; ++i)
+        {
+            temp = Instantiate(baseStalactite, transform);
+            temp.SetActive(false);
+            stalactitePool.Add(temp);
+
+        }
+
+        StartCoroutine(SpikeProduction());
+    }
+
+    IEnumerator SpikeProduction()
+    {
+        while (true)
+        {
+            yield return wait;
+            print("activate one");
+            ActivateFromPool();
+        }
+    }
+
+    GameObject ActivateFromPool()
+    {
+        for(int i = 0; i < stalactitePool.Count; ++i)
+        {
+            if(stalactitePool[i].activeSelf == false)
+            {
+                stalactitePool[i].transform.position = transform.position;
+                stalactitePool[i].SetActive(true);
+                return stalactitePool[i];
+            }
+        }
+
+        GameObject temp = Instantiate(baseStalactite, transform);
+        stalactitePool.Add(temp);
+        return temp;
+    }
+}
